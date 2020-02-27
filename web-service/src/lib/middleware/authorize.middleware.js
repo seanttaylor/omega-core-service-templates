e9d80327f3feed4f9ade0f2d7dfaa08f62f76a67
Sean Travis Taylor <sean.taylor@blackboardinsurance.com>
2020-02-27 03:41:09 +0000

    ADDED ERROR HANDLING AND TOKEN INTROSPECTION TO AUTHORIZATION MIDDLEWARE.

--- web-service/src/lib/middleware/authorize.middleware.js
+++ web-service/src/lib/middleware/authorize.middleware.js
@@ -4,19 +4,40 @@ const AccessControl = require('accesscontrol');
 const jwt = require('jsonwebtoken');
 const ac = new AccessControl(accessGrants.grants);
 
-//This function needs to be curried so we can programatically pre-apply the relevant permission and resource (e.g. 'readAny' and 'beers')
-module.exports = function authorizeRequest(req, res, next) {
-    try {
-        const authToken = req.headers.authorization.split(" ")[1];
-        const decodedToken = jwt.decode(authToken);
-        const permission = ac.can(decodedToken.role[0])['readAny']('beers');
-        console.log(permission.granted);
-        
-    } catch(e) {
-        throw new ServerError({
-            status: 401,
-            error: 'Missing or bad authorization'
-        });
+module.exports = function(action, resource) { 
+    return function authorizeRequest(req, res, next) {
+        try {
+            const authToken = req.headers.authorization.split(" ")[1];
+            const decodedToken = jwt.decode(authToken);
+            const permission = ac.can(decodedToken.role[0])[action](resource);
+            console.log(permission.granted);
+            if (!permission.granted) {
+                throw new ServerError({
+                    status: 403,
+                    error: 'Missing or invalid access grant(s).'
+                });
+            }
+            //The requester has admin privileges.
+            if (permission.granted && decodedToken.role[0] === 'admin') {
+                next();
+                return;
+            }
+            //The request is for a record whose id matches the subject claim on the authorization token.
+            if (req.params.id && req.params.id !== decodedToken.sub) {
+                throw new ServerError({
+                    status: 403,
+                    error: 'Access denied.'
+                });
+            }
+            
+            next();
+            
+        } catch(e) {
+            console.error(e);
+            throw new ServerError({
+                status: e.status || 401,
+                error: e.error || 'Missing or bad authorization.'
+            });
+        }
     }
-    next();
 }
